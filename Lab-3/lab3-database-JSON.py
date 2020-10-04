bc4a98a3f2042d81b95059c08e33278bc5fe01cc
Anish Tankala <atankala@outlook.com>
2020-10-04 16:21:47 -0400

    [Anish] feat:added new table to database

--- Lab-3/lab3-database-JSON.py
+++ Lab-3/lab3-database-JSON.py
@@ -2,6 +2,7 @@ from urllib.request import *
 from urllib.parse import *
 import json
 
+import sqlite3
 # The URL that is formatted: http://api.openweathermap.org/data/2.5/weather?APPID=a808bbf30202728efca23e099a4eecc7&units=imperial&q=ottawa
 # As of October 2015, you need an API key.
 # I have registered under my Carleton email.
@@ -18,17 +19,28 @@ arguments = urlencode(params)
 # Get the weather information
 address = "http://api.openweathermap.org/data/2.5/weather"
 url = address + "?" + arguments
+
 print (url)
 webData = urlopen(url)
 results = webData.read().decode('utf-8')# results is a JSON string
 webData.close()
-
 print (results)
+
+dbconnect =sqlite3.connect("mydatabase.db")
+dbconnect.row_factory = sqlite3.Row
+cursor = dbconnect.cursor()
+
+
 #Convert the json result to a dictionary
-# See http://openweathermap.org/current#current_JSON for the API
+#See http://openweathermap.org/current#current_JSON for the API
 
 data = json.loads(results)# Print the results
 current = data["main"]
+humidity = data["main"]
+
+cursor.execute('''insert into weather(city, humidity) values (?, ?)''', (city, humidity["humidity"]))
+dbconnect.commit();
+
 degreeSym = chr(176)
 
 print ("Temperature: %d%sF" % (current["temp"], degreeSym ))
